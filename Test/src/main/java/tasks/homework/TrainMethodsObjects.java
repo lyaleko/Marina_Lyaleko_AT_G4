5234e15f867d8eb119a15061a97de7e94ac6a4b7
Marina Lyaleko <M_hodanenok@mail.ru>
2021-10-12 08:11:26 +0300

    Added changes to TrainMethodsObjects

--- Test/src/main/java/tasks/homework/TrainMethodsObjects.java
+++ Test/src/main/java/tasks/homework/TrainMethodsObjects.java
@@ -1,10 +1,34 @@
-package basetask;
+package tasks.homework;
 
 public class TrainMethodsObjects {
 
-    public void processMouse(Mouse m){
+    public void  processMouse(Mouse m) {
         System.out.println(m.getName() + m.getAge());
         m.printMouseDetails();
+
+    }
+
+    public void processSouce(Souce s) {
+        System.out.println(s.getName() + s.getColor());
+        s.printSouceDetails();
+
+    }
+
+    public void processBee(Bee b) {
+        System.out.println(b.getName() + b.getGender());
+        b.printBeeDetails();
+
+    }
+
+    public void processObstacle(Obstacle o) {
+        System.out.println(b.getSeverity() + b.getDescription());
+        o.printObstacleDetails();
+
+    }
+
+    public void processPineapple(Pineapple p) {
+        System.out.println(p.setGrade() + b.getHeatCapacity());
+        p.printPineappleDetails();
 
     }
 }
