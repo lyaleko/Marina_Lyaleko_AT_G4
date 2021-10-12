d5dce21fc549e100b2538bfebaa3b77db9d708af
Marina Lyaleko <M_hodanenok@mail.ru>
2021-10-12 20:43:18 +0300

    Added ObjectsRunner

---
+++ Test/src/main/java/tasks/homework/TrainMethodsObjectsRunner.java
@@ -0,0 +1,16 @@
+package tasks.homework;
+
+public class TrainMethodsObjectsRunner {
+    public static void main(String[] args) {
+        TrainMethodsObjects trainMethodsObjects = new TrainMethodsObjects();
+
+        trainMethodsObjects.processMouse(new Mouse("Jerry", 5));
+        trainMethodsObjects.processSouce(new Souce("Hinze", "red"));
+        trainMethodsObjects.processBee(new Bee("man", 123));
+        trainMethodsObjects.processObstacle(new Obstacle("stones", "medium"));
+        trainMethodsObjects.processPineapple(new Pineapple("just grade", 3000);
+
+
+    }
+
+}
