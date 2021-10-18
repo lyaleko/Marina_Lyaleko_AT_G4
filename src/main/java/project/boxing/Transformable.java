c3968555ca235b491e9d3a5745b3763bd09f279b
Marina Lyaleko <M_hodanenok@mail.ru>
2021-10-19 00:22:31 +0300

    Added new classes Vessel,Material and Interfaces Containable,Transformable.

---
+++ src/main/java/project/boxing/Transformable.java
@@ -0,0 +1,6 @@
+package project.boxing;
+
+public interface Transformable implements Water, Sand{
+    void mix();
+
+}
---
+++ src/main/java/project/boxing/Vessel.java
@@ -0,0 +1,18 @@
+package project.boxing;
+
+public abstract class Vessel extends Bottle, Cup, Can{
+
+    private double volume;
+    private double diameter;
+    private int weight;
+    private Material material;
+
+    public Vessel(double volume, double diameter, int weight, Material material) {
+        this.volume = volume;
+        this.diameter = diameter;
+        this.weight = weight;
+        this.material = material;
+    }
+
+
+}
