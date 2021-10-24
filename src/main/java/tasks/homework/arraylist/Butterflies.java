6a775df2b7808bda93aa7ddc5b5ac39062146d30
Marina Lyaleko <M_hodanenok@mail.ru>
2021-10-24 23:31:50 +0300

    Added butterfly

---
+++ src/main/java/tasks/homework/arraylist/Butterflies.java
@@ -0,0 +1,42 @@
+package tasks.homework.arraylist;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Butterflies {
+    public Butterflies() {
+    }
+
+    public static void main(String[] args) {
+        List<String> myList = new ArrayList();
+        myList.add("Common blue");
+        myList.add("Swallowtailai");
+        myList.add("Aglais io");
+        myList.add("Common blue");
+
+        for (String bab : myList) {
+            System.out.print("\"" + bab + "\"" + " ");
+        }
+        System.out.println();
+
+        int count = 0;
+
+        for (String words : myList) {
+            if (words.contains("o")) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+
+        for (int i = 0; i < myList.size(); ++i) {
+            System.out.print(myList.get(i) + " ");
+        }
+        System.out.println();
+
+        for (String str : myList) {
+            System.out.print(str + "\n");
+        }
+
+    }
+}
