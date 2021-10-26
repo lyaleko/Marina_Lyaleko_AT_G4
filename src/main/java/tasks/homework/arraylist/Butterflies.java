6bccdbb3a19b15781a7b64a0bd702386d75f821e
Marina Lyaleko <M_hodanenok@mail.ru>
2021-10-26 23:52:32 +0300

    removed extra constructor

--- src/main/java/tasks/homework/arraylist/Butterflies.java
+++ src/main/java/tasks/homework/arraylist/Butterflies.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Butterflies {
-    public Butterflies() {
-    }
 
     public static void main(String[] args) {
         List<String> myList = new ArrayList();
