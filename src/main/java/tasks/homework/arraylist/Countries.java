6260c785f583e1fea7fb2dda4ec8d0f9c39130c6
Marina Lyaleko <M_hodanenok@mail.ru>
2021-10-26 21:43:19 +0300

    Added Countries

--- src/main/java/tasks/homework/arraylist/Countries.java
+++ src/main/java/tasks/homework/arraylist/Countries.java
@@ -13,17 +13,28 @@ public class Countries {
     public static void main(String[] args) {
 
         List<String> countries = new ArrayList<>();
+
         countries.add("Андора");
         countries.add("Португалия");
         countries.add("Англия");
         countries.add("Замбия");
 
-        for (String country : countries) {
-            for (int i = 0; i < countries.size(); i++) {
-
-            }
-            System.out.print(country + ", ");
+        for (String f : countries) {
         }
+        System.out.println(String.join(", ", countries));
+
+        int countCountries = 0;
+        for (String f : countries) {
+            if (f.length() < 7) {
+                countCountries++;
+            }
+        }
+        System.out.println(countCountries);
+
+        for (int i = 0; i < countries.size(); ++i) {
+            System.out.print(countries.get(i) + " ");
+        }
+        System.out.println();
 
     }
 }
