7af5f899ffe5ff18629bd51e4a856154097568b8
Marina Lyaleko <M_hodanenok@mail.ru>
2021-10-31 15:00:13 +0300

    Added changes to log

--- src/main/java/project/boxing/Warehouse.java
+++ src/main/java/project/boxing/Warehouse.java
@@ -1,5 +1,13 @@
 package project.boxing;
 
-public class Warehouse{
+public class Warehouse extends VesselBox{
 
+    public static void main(String[] args) {
+
+        VesselBox bottle = new VesselBox();
+        VesselBox cup = new VesselBox();
+
+/*        System.out.println(bottle.toString(bottle.toString());
+        System.out.println(cup.toString(cup.toString());*/ //TODO
+    }
 }
