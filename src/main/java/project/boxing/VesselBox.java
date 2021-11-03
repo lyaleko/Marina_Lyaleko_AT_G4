08437e53f07d6591e03c4168379c7ae55adfd6e4
Marina Lyaleko <M_hodanenok@mail.ru>
2021-11-03 21:40:42 +0300

    Added VesselBox and Stocktaking

--- src/main/java/project/boxing/VesselBox.java
+++ src/main/java/project/boxing/VesselBox.java
@@ -1,12 +1,24 @@
 package project.boxing;
 
-// создать класс параметризованного типа (Generic) VesselBox, который может хранить в себе массив
-// из 9, 25 или 36 элементов наследуемых от класса абстрактного класа Vessel.
+import java.io.Serializable;
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
-public class VesselBox<T> extends Vessel{
-    @SuppressWarnings("unchecked")
-    T[][] count = (T[][]) new Object[6][6];
+//создать класс параметризованного типа (Generic) VesselBox, который может
+// хранить в себе массив из 9, 25 или 36 элементов наследуемых от класса абстрактного класа Vessel
 
+public class VesselBox <T extends Vessel> implements Serializable {
+    T [] array;
 
+    public VesselBox(int size, Class<T> type) {
+        this.array = (T[]) Array.newInstance(type, size);
 
+    }
+
+    @Override
+    public String toString() {
+        return "VesselBox{" +
+                "array=" + Arrays.toString(array) +
+                '}';
+    }
 }
