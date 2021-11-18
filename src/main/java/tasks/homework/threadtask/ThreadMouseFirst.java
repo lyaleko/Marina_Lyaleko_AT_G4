ef3fc0bf4dbcf8a33ac1a1024d008129c1ca65de
Marina Lyaleko <M_hodanenok@mail.ru>
2021-11-18 21:16:28 +0300

    Added Array list

--- src/main/java/tasks/homework/threadtask/ThreadMouseFirst.java
+++ src/main/java/tasks/homework/threadtask/ThreadMouseFirst.java
@@ -4,17 +4,22 @@ package tasks.homework.threadtask;
 //-- проитерировать коллекцию и пикнуть каждой мышью
 //- запустить потоки одновременно
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ThreadMouseFirst {
 
     public static void main(String[] args) {
 
         int numThreads = 5;
-        for (int k = 0; k < numThreads; k++) {
+        for (int k = 1; k < numThreads; k++) {
             Thread thread = new Thread(() -> {
+                List<Mouse> mouses = new ArrayList<>();
                 for (int i = 1; i < 21; i++) {
                     Mouse mouse = new Mouse(i);
-                    mouse.peep();
+                    mouses.add(mouse);
                 }
+                mouses.forEach(x -> x.peep());
             });
             thread.start();
         }
--- src/main/java/tasks/homework/threadtask/ThreadMouseSecond.java
+++ src/main/java/tasks/homework/threadtask/ThreadMouseSecond.java
@@ -4,6 +4,9 @@ package tasks.homework.threadtask;
 //-- проитерировать коллекцию и пикнуть каждой четной мышью
 //- запустить потоки
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ThreadMouseSecond {
 
     public static void main(String[] args) {
@@ -11,12 +14,14 @@ public class ThreadMouseSecond {
         int numThreads = 7;
         for (int k = 0; k < numThreads; k++) {
             Thread thread = new Thread(() -> {
+                List<Mouse> mouses = new ArrayList<>();
                 for (int i = 1; i < 19; i++) {
                     Mouse mouse = new Mouse(i);
                     if (i%2==0) {
-                        mouse.peep();
+                        mouses.add(mouse);
                     }
                 }
+                mouses.forEach(x -> x.peep());
             });
             thread.start();
         }
