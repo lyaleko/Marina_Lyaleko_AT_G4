97a0031bb7bbd64503d59f6c91911ec53dfb53b5
Marina Lyaleko <M_hodanenok@mail.ru>
2021-11-20 00:19:54 +0300

    Added formula for weight, removed int weight from input values

--- src/main/java/project/vessel/Vessel.java
+++ src/main/java/project/vessel/Vessel.java
@@ -1,4 +1,16 @@
-package project.boxing;
+package project.vessel;
+/* **- abstract class Vessel (extending classes Bottle, Cup, Can)
+- each Vessel has:
+        -- private double volume;
+        -- private double diameter;
+        -- private int weight;
+        -- private Material material;
+        -- all field are set in costructor*/
+
+//---- volume, diameter, material are passed from child constructor
+//---- weight is set in constructor as density * volume / 4
+//---- all fields have getters and setters
+
 
 import project.material.Material;
 
@@ -9,10 +21,10 @@ public abstract class Vessel{
     private int weight;
     private Material material;
 
-    public Vessel(double volume, double diameter, int weight, Material material) {
+    public Vessel(double volume, double diameter, Material material) {
         this.volume = volume;
         this.diameter = diameter;
-        this.weight = weight;
+        this.weight = (int) (volume*material.getDensity()/4);
         this.material = material;
     }
 
