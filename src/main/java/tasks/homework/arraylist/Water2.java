f118c43ff3d301b98f30bbb5d2a3cc351449edbf
Marina Lyaleko <M_hodanenok@mail.ru>
2021-11-20 00:30:21 +0300

    Fixed ArrayList home tasks due some changes in Water Class (project package)

--- src/main/java/tasks/homework/arraylist/Water2.java
+++ src/main/java/tasks/homework/arraylist/Water2.java
@@ -1,26 +1,38 @@
-/*создать абстрактный класс Water
+/* **создать абстрактный класс Water
 у воды есть такие характеристики, цвет, прозрачность, запах, температура
+
 создать класс SparklingWater, являющийся дочерним Water
 у газировки есть пузырьки
 вода заполняется пузырьками при упаковке на заводе, для этого у нее есть метод pump(Bubble[] bubbles)
 1 литр воды содержит 10 тыс пузырьков
 у газировки есть метод degas(), который удаляет пузырьки по одному и вызывает их лопанье*/
-package project.liquid;
 
-import project.feature.Transformable;
+/* **содеждит приватные поля String color = "no", String transparency = "transparent", String smell = "no", int temperature = 0
+содеждит геттеры и сеттеры ко всем полям*/
 
-public abstract class Water implements Transformable {
+/*- abstract class Water
+---- private String color = "no", String smell = "no", int temperature = 0
+---- all fields have getters and setters*/
+package tasks.homework.arraylist;
+
+
+
+public class Water2 {
 
     private String color = "no";
     private String transparency = "transparent";
     private String smell = "no";
     private int temperature = 0;
 
-    public Water() {
+    public Water2() {
         this.color = color;
         this.transparency = transparency;
         this.smell = smell;
         this.temperature = temperature;
+    }
+
+    public Water2(String прозрачная, String нет) {
+
     }
 
     public String getColor() {
@@ -55,4 +67,3 @@ public abstract class Water implements Transformable {
         this.temperature = temperature;
     }
 }
-
