b1e6e7870873a8838d90f251298d4a6c8988f370
Marina Lyaleko <M_hodanenok@mail.ru>
2021-11-20 00:10:58 +0300

    Set values in Glass, Plastic, Metal

--- src/main/java/project/material/Glass.java
+++ src/main/java/project/material/Glass.java
@@ -1,8 +1,14 @@
 package project.material;
 
+/* **- classes Bottle, Cup, Can, Glass, Plastic, Metal have constructors, which set all fields with default values
+        - all fields should have gettres and setters*/
+
+//  Glass (thermalConductivity - 0.017, color - green, density - 2200)
+
 public class Glass extends Material{
 
     public Glass(double thermalConductivity, String color, double density) {
-        super(thermalConductivity, color, density);
+        super(0.017, "green", 2200);
     }
+
 }
